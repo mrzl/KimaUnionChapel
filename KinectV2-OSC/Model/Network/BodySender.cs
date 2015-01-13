0ba1eac93bbf89951ed49c6e3ef87eea3d266d9d
Marcel Schwittlick <lzrm@mrzl.net>
2015-01-13 23:27:02 +0100

    adds kinectv2-osc addon

---
+++ KinectV2-OSC/Model/Network/BodySender.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Microsoft.Kinect;
+using Rug.Osc;
+
+namespace KinectV2OSC.Model.Network
+{
+    public class BodySender
+    {
+        private MessageBuilder messageBuilder;
+        private List<OscSender> oscSenders;
+        private List<IPAddress> ipAddresses;
+        private OscMessage message;
+        private string port;
+        private string status;
+
+        public BodySender(string delimitedIpAddresses, string port)
+        {
+            this.status = "";
+            this.ipAddresses = this.Parse(delimitedIpAddresses);
+            this.oscSenders = new List<OscSender>();
+            this.port = port;
+            this.messageBuilder = new MessageBuilder();
+            this.TryConnect();
+        }
+
+        private void TryConnect()
+        {
+            foreach(var ipAddress in this.ipAddresses)
+            {
+                try
+                {
+                    var oscSender = new OscSender(ipAddress, int.Parse(this.port));
+                    oscSender.Connect();
+                    this.oscSenders.Add(oscSender);
+                    this.status += "OSC connection established on\nIP: " + ipAddress + "\nPort: " + port + "\n";
+                }
+                catch (Exception e)
+                {
+                    this.status += "Unable to make OSC connection on\nIP: " + ipAddress + "\nPort: " + port + "\n";
+                    Console.WriteLine("Exception on OSC connection...");
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+
+        }
+
+        public void Send(Body[] bodies)
+        {
+            foreach (Body body in bodies)
+            {
+                if (body.IsTracked)
+                {
+                    this.Send(body);
+                }
+            }
+        }
+
+        public string GetStatusText()
+        {
+            return this.status;
+        }
+
+        private void Send(Body body)
+        {
+            foreach (var joint in body.Joints)
+            {
+                message = messageBuilder.BuildJointMessage(body, joint);
+                this.Broadcast(message);
+            }
+
+            message = messageBuilder.BuildHandMessage(body, "Left", body.HandLeftState, body.HandLeftConfidence);
+            this.Broadcast(message);
+
+            message = messageBuilder.BuildHandMessage(body, "Right", body.HandRightState, body.HandRightConfidence);
+            this.Broadcast(message);
+        }
+
+        private void Broadcast(OscMessage message)
+        {
+            foreach (var oscSender in this.oscSenders)
+            {
+                oscSender.Send(message);
+            }
+        }
+
+        private List<IPAddress> Parse(string delimitedIpAddresses)
+        {
+            try
+            {
+                var ipAddressStrings = delimitedIpAddresses.Split(',');
+                var ipAddresses = new List<IPAddress>();
+                foreach (var ipAddressString in ipAddressStrings)
+                {
+                    ipAddresses.Add(IPAddress.Parse(ipAddressString));
+                }
+                return ipAddresses;
+            }
+            catch (Exception e)
+            {
+                status += "Unable to parse IP address string: '" + delimitedIpAddresses + "'";
+                Console.WriteLine("Exception parsing IP address string...");
+                Console.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+    }
+}
---
+++ KinectV2-OSC/Model/Network/MessageBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Rug.Osc;
+
+namespace KinectV2OSC.Model.Network
+{
+    public class MessageBuilder
+    {
+        public OscMessage BuildJointMessage(Body body, KeyValuePair<JointType, Joint> joint)
+        {
+            var address = String.Format("/bodies/{0}/joints/{1}", body.TrackingId, joint.Key);
+            var position = joint.Value.Position;
+            //System.Diagnostics.Debug.WriteLine(address);
+            return new OscMessage(address, position.X, position.Y, position.Z, joint.Value.TrackingState.ToString());
+        }
+
+        public OscMessage BuildHandMessage(Body body, string key, HandState state, TrackingConfidence confidence)
+        {
+            var address = String.Format("/bodies/{0}/hands/{1}", body.TrackingId, key);
+            //System.Diagnostics.Debug.WriteLine(address);
+            return new OscMessage(address, state.ToString(), confidence.ToString());
+        }
+    }
+}
