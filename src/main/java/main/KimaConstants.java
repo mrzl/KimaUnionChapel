3131088a45daa6c090c0bf9dd95b9e4fbf0e2b71
Marcel Schwittlick <marzzzel@gmail.com>
2015-02-03 20:16:56 +0100

    adjusts min/max frequency values

--- src/main/java/main/KimaConstants.java
+++ src/main/java/main/KimaConstants.java
@@ -7,10 +7,12 @@ public class KimaConstants {
     // attack = [0,1}
     // frequency = [200,10000]
     // amplitude = [0,0.99]
-    public static final int MIN_FREQUENCY = 20;
-    public static final int MAX_FREQUENCY = 20000;
+    public static final int MIN_FREQUENCY = 200;
+    public static final int MAX_FREQUENCY = 12000;
     public static final float MIN_AMPLITUDE = 0.0f;
-    public static final float MAX_AMPLITUDE = 0.99f;
+    public static final float MAX_AMPLITUDE = 40.0f;
+    public static final int MIN_ATTACK = 0;
+    public static final int MAX_ATTACK = 1;
 
     public static final int MIN_MIDI = 0;
     public static final int MAX_MIDI = 127;
