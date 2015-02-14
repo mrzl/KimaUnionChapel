905ced70496648d33b45500a6856e5440f8e0c7a
Marcel Schwittlick <marzzzel@gmail.com>
2015-02-14 21:44:05 +0100

    adds parameters MARE_UNDARUM, AXIS_MUNDI, AURORA

--- src/main/java/midi/VisualParameterEnum.java
+++ src/main/java/midi/VisualParameterEnum.java
@@ -4,6 +4,10 @@ package midi;
  * Created by mrzl on 16.01.2015.
  */
 public enum VisualParameterEnum {
+    MARE_UNDARUM,
+    AXIS_MUNDI,
+    AURORA,
+
     UPDATE_DELAY,
     MIN_HUE,
     MAX_HUE,
