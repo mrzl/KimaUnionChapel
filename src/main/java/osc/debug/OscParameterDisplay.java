077bfd91d77bfe939d678e22a4d008c73f3a21cc
Marcel Schwittlick <marzzzel@gmail.com>
2015-02-14 18:43:03 +0100

    displaying new parameters in debug window

--- src/main/java/osc/debug/OscParameterDisplay.java
+++ src/main/java/osc/debug/OscParameterDisplay.java
@@ -35,6 +35,18 @@ public class OscParameterDisplay extends PApplet {
         Slider attackSlider2 = cp5.addSlider( "AT2" ).setPosition( 285, 10 ).setSize( 15, 100 ).setRange( KimaConstants.ATTACK_MIN, KimaConstants.ATTACK_MAX ).setValue( 0.0f );
         Slider attackSlider3 = cp5.addSlider( "AT3" ).setPosition( 320, 10 ).setSize( 15, 100 ).setRange( KimaConstants.ATTACK_MIN, KimaConstants.ATTACK_MAX ).setValue( 0.0f );
 
+        Slider peakSlider1 = cp5.addSlider( "PEAK1" ).setPosition( 380, 10 ).setSize( 15, 100 ).setRange( KimaConstants.PEAK_MIN, KimaConstants.PEAK_MAX ).setValue( 0.0f );
+        Slider peakSlider2 = cp5.addSlider( "PEAK2" ).setPosition( 415, 10 ).setSize( 15, 100 ).setRange( KimaConstants.PEAK_MIN, KimaConstants.PEAK_MAX ).setValue( 0.0f );
+        Slider peakSlider3 = cp5.addSlider( "PEAK3" ).setPosition( 450, 10 ).setSize( 15, 100 ).setRange( KimaConstants.PEAK_MIN, KimaConstants.PEAK_MAX ).setValue( 0.0f );
+
+        Slider fundamentalSlider1 = cp5.addSlider( "FU1" ).setPosition( 500, 10 ).setSize( 15, 100 ).setRange( KimaConstants.FUNDAMENTAL_MIN, KimaConstants.FUNDAMENTAL_MAX).setValue( 0.0f );
+        Slider fundamentalSlider2 = cp5.addSlider( "FU2" ).setPosition( 535, 10 ).setSize( 15, 100 ).setRange( KimaConstants.FUNDAMENTAL_MIN, KimaConstants.FUNDAMENTAL_MAX ).setValue( 0.0f );
+        Slider fundamentalSlider3 = cp5.addSlider( "FU3" ).setPosition( 570, 10 ).setSize( 15, 100 ).setRange( KimaConstants.FUNDAMENTAL_MIN, KimaConstants.FUNDAMENTAL_MAX ).setValue( 0.0f );
+
+        Slider newnoteSlider1 = cp5.addSlider( "NN1" ).setPosition( 620, 10 ).setSize( 15, 100 ).setRange( KimaConstants.NEWNOTE_MIN, KimaConstants.NEWNOTE_MAX ).setValue( 0.0f );
+        Slider newnoteSlider2 = cp5.addSlider( "NN2" ).setPosition( 655, 10 ).setSize( 15, 100 ).setRange( KimaConstants.NEWNOTE_MIN, KimaConstants.NEWNOTE_MAX ).setValue( 0.0f );
+        Slider newnoteSlider3 = cp5.addSlider( "NN3" ).setPosition( 690, 10 ).setSize( 15, 100 ).setRange( KimaConstants.NEWNOTE_MIN, KimaConstants.NEWNOTE_MAX ).setValue( 0.0f );
+
         parameters.put( SoundInputParameterEnum.AMPLITUDE_PARAMETER1, amplitudeSlider1 );
         parameters.put( SoundInputParameterEnum.AMPLITUDE_PARAMETER2, amplitudeSlider2 );
         parameters.put( SoundInputParameterEnum.AMPLITUDE_PARAMETER3, amplitudeSlider3 );
@@ -44,6 +56,15 @@ public class OscParameterDisplay extends PApplet {
         parameters.put( SoundInputParameterEnum.ATTACK_PARAMETER1, attackSlider1 );
         parameters.put( SoundInputParameterEnum.ATTACK_PARAMETER2, attackSlider2 );
         parameters.put( SoundInputParameterEnum.ATTACK_PARAMETER3, attackSlider3 );
+        parameters.put( SoundInputParameterEnum.PEAK_PARAMETER1, peakSlider1 );
+        parameters.put( SoundInputParameterEnum.PEAK_PARAMETER2, peakSlider2 );
+        parameters.put( SoundInputParameterEnum.PEAK_PARAMETER3, peakSlider3 );
+        parameters.put( SoundInputParameterEnum.FUNDAMENTAL_PARAMETER1, fundamentalSlider1 );
+        parameters.put( SoundInputParameterEnum.FUNDAMENTAL_PARAMETER2, fundamentalSlider2 );
+        parameters.put( SoundInputParameterEnum.FUNDAMENTAL_PARAMETER3, fundamentalSlider3 );
+        parameters.put( SoundInputParameterEnum.NEWNOTE_PARAMETER1, newnoteSlider1 );
+        parameters.put( SoundInputParameterEnum.NEWNOTE_PARAMETER2, newnoteSlider2 );
+        parameters.put( SoundInputParameterEnum.NEWNOTE_PARAMETER3, newnoteSlider3 );
 
     }
 
