76f4b0f3930856d9ca2bab0e3413ebb38f604de2
Marcel Schwittlick <marzzzel@gmail.com>
2015-02-14 22:09:03 +0100

    adding all buttons to the switch/vase

--- src/main/java/midi/bcr2000/BnrController.java
+++ src/main/java/midi/bcr2000/BnrController.java
@@ -108,10 +108,40 @@ public class BnrController {
                 return BcrKnobEnum.KNOB_4_7;
             case 104:
                 return BcrKnobEnum.KNOB_4_8;
+
+
             case 65:
                 return BcrKnobEnum.BUTTON_1_1;
             case 66:
                 return BcrKnobEnum.BUTTON_1_2;
+            case 67:
+                return BcrKnobEnum.BUTTON_1_3;
+            case 68:
+                return BcrKnobEnum.BUTTON_1_4;
+            case 69:
+                return BcrKnobEnum.BUTTON_1_5;
+            case 70:
+                return BcrKnobEnum.BUTTON_1_6;
+            case 71:
+                return BcrKnobEnum.BUTTON_1_7;
+            case 72:
+                return BcrKnobEnum.BUTTON_1_8;
+            case 73:
+                return BcrKnobEnum.BUTTON_2_1;
+            case 74:
+                return BcrKnobEnum.BUTTON_2_2;
+            case 75:
+                return BcrKnobEnum.BUTTON_2_3;
+            case 76:
+                return BcrKnobEnum.BUTTON_2_4;
+            case 77:
+                return BcrKnobEnum.BUTTON_2_5;
+            case 78:
+                return BcrKnobEnum.BUTTON_2_6;
+            case 79:
+                return BcrKnobEnum.BUTTON_2_7;
+            case 80:
+                return BcrKnobEnum.BUTTON_2_8;
             default:
                 System.err.println( "ERROR: Buttons not yet mapped, something is wrong." );
                 return BcrKnobEnum.BUTTON_1_1;
